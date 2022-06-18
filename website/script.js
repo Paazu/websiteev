791b370545bf5cbbf71853fcb201ea9bbd582598
Paaz_ <felke2101@gmail.com>
2022-06-18 12:21:45 +0200

    v0.0.2

---
+++ website/script.js
@@ -0,0 +1,5 @@
+let header      = document.querySelector("header");
+let atOpening   = document.querySelector("atopening");
+let headText    = document.querySelector("head-text");
+let subHeadText = document.querySelector("subhead-text");
+let profileCard = document.querySelector("profile-card");
